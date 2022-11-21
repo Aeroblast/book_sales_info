7fd793c7e2490064e1bfdc053f4e1b3391c0563d
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2022-11-21 15:40:32 +0900

    feat: JSON patch generator

--- webapp/src/components/RankEntry.vue
+++ webapp/src/components/RankEntry.vue
@@ -1,4 +1,15 @@
 <template>
+  <div class="editor">
+    <span class="trigger" @click="editor = !editor; detail = editor" title="更新用JSON"></span>
+    <span class="editor_title" :data-editor="editor">更新用JSONを生成</span>
+    <div class="editor_area" :data-editor="editor">
+      部数詳細：<input type="text" v-model="editor_salesDesc" /><span>{{ SearchAsianNumber(editor_salesDesc) }}</span><br>
+      &#x3000;&#x3000;日付：<input type="date" v-model="editor_date" /><br>
+      情報元(MarkDown)：<br><textarea v-model="editor_sourceDesc">[]()</textarea><br>
+      情報元(Preview)：<br><span v-html="MarkDown(editor_sourceDesc)"></span><br>
+      <button @click="TryCopy">Copy</button><span v-html="copy_result"></span>
+    </div>
+  </div>
   <div class="entry" :data-detail="detail">
     <span class="salesValue">{{ AsianNumber(entry.salesValue) }}部</span>&nbsp;&nbsp;
     <span class="title" @click="detail = !detail" v-html="titleDisplayHTML"></span>&nbsp;&nbsp;
@@ -22,6 +33,11 @@ export default {
   data() {
     return {
       detail: false,
+      editor: false,
+      editor_salesDesc: "",
+      editor_sourceDesc: "[]()",
+      editor_date: null,
+      copy_result: ""
     };
   },
   mounted() {
@@ -43,9 +59,49 @@ export default {
         return `${num / 10000}万${thousand}`;
       }
     },
+    SearchAsianNumber(str) {
+      let m = str.match(/([0-9]+)万(([0-9]+)千)?/);
+      if (!m) return 0;
+      let value = 0;
+      if (m[3]) { value += parseInt(m[3]) * 1000; }
+      if (m[1]) { value += parseInt(m[1]) * 10000; }
+      return value;
+    },
     MarkDown(raw) {
       return raw.replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2'>$1</a>");
     },
+    TryCopy() {
+      let r = {};
+      r.salesDesc = this.editor_salesDesc.trim();
+      if (!r.salesDesc) {
+        this.copy_result = "Error: SalesDesc is empty!";
+        return;
+      }
+      r.recordDate = this.editor_date;
+      if (!r.recordDate) {
+        this.copy_result = "Error: recordDate is empty!";
+        return;
+      }
+      if (r.recordDate.localeCompare(this.entry.recordDate) <= 0) {
+        this.copy_result = "Error: New date should not earlier than last record!";
+        return;
+      }
+      r.sourceDesc = this.editor_sourceDesc.trim();
+      if (!r.sourceDesc) {
+        this.copy_result = "Error: SourceDesc is empty!";
+        return;
+      }
+      r.salesValue = this.SearchAsianNumber(r.salesDesc);
+      r.isbn = this.entry.isbn;
+      r.title = this.entry.title;
+      let jsonText = JSON
+        .stringify(r, ["title", "salesValue", "recordDate", "salesDesc", "sourceDesc", "isbn"], 2)
+        .replaceAll('\n', '\n  ');
+      console.log(jsonText);
+      navigator.clipboard.writeText(jsonText).then(() => {
+        this.copy_result = "Success!";
+      });
+    }
   },
   computed: {
     isLongTitle() {
@@ -105,6 +161,7 @@ export default {
   overflow: hidden;
   max-height: 0;
   transition: max-height 0.5s;
+  position: relative;
 }
 
 .detail>p {
@@ -123,6 +180,46 @@ export default {
 
 .expander {
   color: rgb(136, 116, 255);
+}
+
+.editor {
+  position: relative;
+  height: 0;
+  left: 100%;
+  overflow: visible;
+  font-size: 0.8em;
+}
+
+.editor>.trigger {
+  width: 1em;
+  height: 1em;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.editor>.trigger:hover {
+  background-color: rgb(162, 162, 162);
+}
+
+.editor_area {
+  display: block;
+  overflow: visible;
+}
+
+.editor_area>input[type="text"] {
+  width: 10em;
+}
+
+.editor_area>textarea {
+  width: 15em;
+}
+
+.editor_title[data-editor="false"] {
+  display: none;
+}
+
+.editor_area[data-editor="false"] {
+  display: none;
 }
 
 @media (max-width: 600px) {
