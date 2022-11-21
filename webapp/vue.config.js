e5a1e73ca75a9a6eee0f8b7974e8ab23fb85e439
Aeroblast <15964528+Aeroblast@users.noreply.github.com>
2022-11-21 18:11:05 +0900

    build change

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '',
-  outputDir: 'docs'
+  outputDir: '../docs'
 })
